5485b6f263ba7431ffc2b68248bcf30355a782a5
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-08-12 20:41:30 -0300

    chamando rotas do controller

--- src/routes/cartas.routes.js
+++ src/routes/cartas.routes.js
@@ -1,1 +1,18 @@
-const express = require("express");
+const express = require("express");const express = require("express");
+const chamandoUsuarioApelidoController = require("../controllers/cartas/acumularCartasController");
+const criandoUsuarioController = require("../controllers/cartas/cadastraCartasController");
+const deletandoUsuarioController = require("../controllers/cartas/compararCartasController")
+const mudandoUsuarioController = require("../controllers/cartas/pegarCartaController");
+const mudandoUsuarioController = require("../controllers//cartas/pegarListaDeCartaControoller");
+
+const usuarioRoute = express.Router();
+
+usuarioRoute.post("/usuario", criandoUsuarioController);
+
+usuarioRoute.get("/usuario/:apelido", chamandoUsuarioApelidoController);
+
+usuarioRoute.put("/usuario/:id", mudandoUsuarioController);
+
+usuarioRoute.delete('/usuario/:id',deletandoUsuarioController);
+
+module.exports = usuarioRoute;
