6978f86f1d2a423371de78ff12096bcb53c478da
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-08-12 09:36:15 -0300

    fix - colunas do banco e retorno disso

--- src/controllers/cartas/cadastraCartasController.js
+++ src/controllers/cartas/cadastraCartasController.js
@@ -1,5 +1,11 @@
 const criandoCartasController = async(req,res) =>{
     let db = require("../../connection/db");
     let carta = require('../../models/cartas');
-
+    await db.sync()
+    const {name, type, attributes, hp, attack, defense, special_attack, special_defense, speed} = req.body;
+    const novaCarta = await carta.create({
+        name, type, attributes, hp, attack, defense, special_attack, special_defense, speed
+    });
+    return res.json({carta: novaCarta});
 }
+
