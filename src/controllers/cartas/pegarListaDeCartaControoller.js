2d4782930031d2149ab726908598463a3c0a7733
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-08-12 09:47:28 -0300

    fix - esperar pegar no banco e retornar

--- src/controllers/cartas/pegarListaDeCartaControoller.js
+++ src/controllers/cartas/pegarListaDeCartaControoller.js
@@ -1,3 +1,6 @@
 const pegarListaDeCartaController = async (req, res) =>{
     let carta = require("../../models/cartas")
+    const listaDecartas = await carta.findAll();
+    return res.status(200).json(listaDecartas)
 }
+module.exports =  pegarListaDeCartaController;
