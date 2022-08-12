d930bfafbf895feda01165cc36d854faffb8bec2
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-08-12 10:18:05 -0300

    fix - variavel pegar carta pelo ome e retornar

--- src/controllers/cartas/pegarCartaController.js
+++ src/controllers/cartas/pegarCartaController.js
@@ -1,3 +1,7 @@
 const pegarCartaController = async (req, res) => {
-  
-} 
+  carta = require ("../../models/cartas")
+  const Name = req.params.name
+  const cartaPeloNome = await carta.findOne({ where: { name: Name } });
+  return res.status(200).json({ cartaPeloNome: cartaPeloNome }
+)};
+module.exports = pegarCartaController; 
