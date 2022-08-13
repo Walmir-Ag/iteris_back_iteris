2330883e09ea6c0bb4df5bb3e845b71285bc76d1
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-08-13 12:26:28 -0300

    fix - tentativa de lógiva para comparar

--- src/controllers/cartas/compararCartasController.js
+++ src/controllers/cartas/compararCartasController.js
@@ -1,4 +1,9 @@
+
 const compararCartasController = async (req, res)=>{
-    const carta = require("../../models/cartas")
+    let carta = require("../../models/cartas")
+    const pegarPelome = req.params.name
+    atributes: [ hp, attack, defense, special_attack, special_defense, speed ] = req.body
+    const compararAtributos = await carta.findAll({limit: 2, where: { comparararPeloname: pegarPelome }})
+    
     
 }
