cb95677593db99611f8122dec0b6c3152c711771
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-08-13 06:47:27 -0300

    fix- tirando "atributes" do criar cartas

--- src/controllers/cartas/cadastraCartasController.js
+++ src/controllers/cartas/cadastraCartasController.js
@@ -2,9 +2,9 @@ const criandoCartasController = async(req,res) =>{
     let db = require("../../connection/db");
     let carta = require('../../models/cartas');
     await db.sync()
-    const {name, type, attributes, hp, attack, defense, special_attack, special_defense, speed} = req.body;
+    const {name, type, hp, attack, defense, special_attack, special_defense, speed} = req.body;
     const novaCarta = await carta.create({
-        name, type, attributes, hp, attack, defense, special_attack, special_defense, speed
+        name, type, hp, attack, defense, special_attack, special_defense, speed
     });
     return res.json({carta: novaCarta});
 }
