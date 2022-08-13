6de79137f2579a57b827f3c1509aaa67f60b6eef
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-08-13 07:41:17 -0300

    fix - corregindo erro de forma de requerir ortas

--- src/routes/cartas.routes.js
+++ src/routes/cartas.routes.js
@@ -1,20 +1,20 @@
 const express = require("express");
-const acumularCartasController = require("../controllers/cartas/acumularCartasController");
-const cadastraCartasController = require("../controllers/cartas/cadastrarCartasController");
-const compararCartasController = require("../controllers/cartas/compararCartasController");
-const pegarCartaController = require("../controllers/cartas/pegarCartaController");
-const pegarListaDeCartaController = require("../controllers//cartas/pegarListaDeCartaController");
+//const acumularCartasController = require ("../controllers/cartas/acumularCartasController");
+const cadastrarCartasController = require ("../controllers/cartas/cadastrarCartasController")
+const compararCartasController = require ("../controllers/cartas/pegarCartaController")
+const pegarCartaController = require ("../controllers/cartas/pegarCartaController");
+const pegarListaDeCartaController = require ("../controllers/cartas/pegarListaDeCartaControoller");
 
 const cartasRoute = express.Router();
 
-cartasRoute.post("/cartas", cadastraCartasController);
+cartasRoute.post("/cartas", cadastrarCartasController);
 
 cartasRoute.get("/cartas/nome/:Name", pegarCartaController);
 
-cartasRoute.get("/cartas/lista/", pegarListaDeCartaController);
+cartasRoute.get("/cartas/lista", pegarListaDeCartaController);
 
-cartasRoute.get("/cartas/compararatributos/", compararCartasController);
+cartasRoute.get("/cartas/compararatributos/:Name", compararCartasController);
 
-cartasRoute.get("/cartas/acumular/", acumularCartasController)
+//cartasRoute.get("/cartas/acumular/", acumularCartasController)
 
 module.exports = cartasRoute;
