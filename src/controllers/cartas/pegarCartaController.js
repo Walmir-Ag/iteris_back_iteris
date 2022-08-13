6f4ad5da0a8e93598c82415e184fdc43d637d474
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-08-13 12:27:01 -0300

    fix - correção na variavel "name"

--- src/controllers/cartas/pegarCartaController.js
+++ src/controllers/cartas/pegarCartaController.js
@@ -1,7 +1,7 @@
 const pegarCartaController = async (req, res) => {
   let carta = require ("../../models/cartas")
-  const Name = req.params.name
-  const cartaPeloNome = await carta.findOne({ where: { pegar: Name } });
+  const name = req.params.name
+  const cartaPeloNome = await carta.findOne({ where: { pegar: name } });
   return res.status(200).json({ pegar: cartaPeloNome }
 )};
 module.exports = pegarCartaController; 
