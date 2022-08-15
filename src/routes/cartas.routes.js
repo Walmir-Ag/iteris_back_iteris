6e7e9f8e2e75abe745684f7dc4b5433c3fb0b6f0
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-08-15 11:47:01 -0300

    fix - melhorando o codigo e corrigindo erro nos gets

--- src/routes/cartas.routes.js
+++ src/routes/cartas.routes.js
@@ -1,21 +1,13 @@
 const express = require("express");
-//const acumularCartasController = require ("../controllers/cartas/acumularCartasController");
-const cadastrarCartasController = require ("../controllers/cartas/cadastrarCartasController")
-const compararCartasController = require ("../controllers/cartas/pegarCartaController")
+const cadastrarCartasController = require ("../controllers/cartas/cadastrarCartasController");
 const pegarCartaController = require ("../controllers/cartas/pegarCartaController");
 const pegarListaDeCartaController = require ("../controllers/cartas/pegarListaDeCartaControoller");
 
 const cartasRoute = express.Router();
 
 cartasRoute.post("/cartas", cadastrarCartasController);
-
-cartasRoute.get("/cartas/:name", pegarCartaController);
-
-cartasRoute.get("/cartas/lista", pegarListaDeCartaController);
-
-cartasRoute.get("/cartas/compararatributos/", compararCartasController);
-
-//cartasRoute.get("/cartas/acumular/", acumularCartasController)
+cartasRoute.get("/cartas/:id", pegarCartaController);
+cartasRoute.get("/listar", pegarListaDeCartaController);
 
 module.exports = cartasRoute;
 
