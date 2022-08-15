1c28dd35b0581a4461e8040d94b7655bf4c2be4e
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-08-15 11:45:02 -0300

    fix - corrigindo erros

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
 require('dotenv/config')
 const express = require('express')
-const cartasRoute = require('./routes/cartas.routes')
+const cartasRoute = require('./routes/cartas.routes');
 const app = express()
-const port = 8089;
+const port = 8082;
 
 app.use(express.json())
-
 app.use(cartasRoute);
 
 app.listen(port, () => {
